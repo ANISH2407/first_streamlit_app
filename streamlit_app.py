2c8663bde5e272e294069c545f0e4c3fbc32136d
ANISH2407 <109344450+ANISH2407@users.noreply.github.com>
2022-07-15 17:20:44 +0530

    Update streamlit_app.py
    added additional text in body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale,Spinach and Rocket smoothie')
+streamlit.text('Hard-Boiled Free-range egg')
